a19abfb5b28c987c11f0cffeb513eb5572807522
kamilah santos <kahsantos89@gmail.com>
2020-06-04 21:12:22 -0300

    criando a aplicacao

---
+++ src/test/java/com/dio/livecoding/crud/salasdereuniao/SalasdereuniaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dio.livecoding.crud.salasdereuniao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalasdereuniaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
